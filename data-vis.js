d7478849466c47b1d9f4f4d1f3166d312b1162eb
Lucas Theiss <theiss.lucas@icloud.com>
2022-11-13 14:04:24 +0100

    Added scatter-plot implementation

--- data-vis.js
+++ data-vis.js
@@ -1,64 +1,34 @@
 const getDataMethodNameAttribute = 'dataLoader';
 let getDataMethodName;
 let shadow;
-const dataDiv = document.createElement("div");
-// const plotlyImport = document.createElement('script');
-
-class DataVis extends HTMLElement {
-    constructor() {
-        super();
-        shadow = this.attachShadow({mode: 'open'});
-        shadow.innerHTML = "<div id='shadowDiv'></div>";
-        loadExternalJS("https://cdn.plot.ly/plotly-2.16.1.min.js");
-        addElementsToShadowDom();
-        getDataMethodName = this.getAttribute(getDataMethodNameAttribute);
-    }
-}
-
-function addElementsToShadowDom() {
-    const shadowDiv = shadow.getElementById("shadowDiv");
-    dataDiv.id = "dataDiv";
-    // plotlyImport.src = "https://cdn.plot.ly/plotly-2.16.1.min.js";
-    // shadowDiv.appendChild(plotlyImport);
-    // document.getElementsByTagName('head')[0].appendChild(plotlyImport)
-    shadowDiv.appendChild(dataDiv);
-}
-
+const dataDiv = createDataDiv();
 window.addEventListener('load',
     () => {
         window.customElements.define("data-vis", DataVis);
         tryToGetData();
     }, false);
 
+function createDataDiv() {
+    let div = document.createElement("div");
+    div.id = "dataDiv";
+    return div;
+}
+
+class DataVis extends HTMLElement {
+    constructor() {
+        super();
+        shadow = this.attachShadow({mode: 'open'});
+        shadow.innerHTML = "<div id='shadowDiv'></div>";
+        shadow.getElementById("shadowDiv").appendChild(dataDiv)
+        getDataMethodName = this.getAttribute(getDataMethodNameAttribute);
+    }
+}
+
 function tryToGetData() {
     let getDataFunction = window[getDataMethodName];
     getDataFunction().then(r => {
-        console.log(r);
-        plotData(r);
+        validateData(r);
+
+        analyzeAndVisualizeData(r, dataDiv);
     });
-}
-
-function plotData(data) {
-    Plotly.newPlot(dataDiv, data);
-}
-
-/**
- * used to load plotly.js code
- * should be replaced by a better method to load external js code
- * @param TARGET_URL
- */
-function loadExternalJS(TARGET_URL){
-    let xhr = window.ActiveXObject ? new ActiveXObject("Microsoft.XMLHTTP") : new XMLHttpRequest();
-    xhr.open('GET', TARGET_URL, false);
-    xhr.send(null);
-    let code = xhr.responseText;
-    let dScript = document.createElement('script');
-    try {
-        dScript.appendChild( document.createTextNode(code) );
-        document.getElementsByTagName('head')[0].appendChild(dScript);
-    } catch(e) {
-        dScript.text = code;
-        document.getElementsByTagName('head')[0].appendChild(dScript);
-    }
-    xhr = null;
 }
